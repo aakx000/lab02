b1081bdc847ad1f830995d413b00e6347ecdd770
aakx000 <aakx0506@gmail.com>
2024-04-28 19:57:35 +0300

    add hello_world.cpp v2.0

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-cout << "Hello, world!" << endl;
-return 0;
+	string name;
+	getline(cin, name);
+	cout << "Hello world from " << name << endl;
+	return 0;
 }
