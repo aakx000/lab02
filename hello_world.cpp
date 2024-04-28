b80e411212b5b4c784819b5fd7b4aa316a51482e
aakx000 <aakx0506@gmail.com>
2024-04-28 20:07:25 +0300

    removed using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main() {
-	string name;
-	getline(cin, name);
-	cout << "Hello world from " << name << endl;
+	std::string name;
+	std::getline(cin, name);
+	std::cout << "Hello world from " << name << std::endl;
 	return 0;
 }
